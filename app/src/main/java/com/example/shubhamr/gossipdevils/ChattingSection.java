7a8776c1a138b6e1c0cbd56d81ce3b80e48c6d10
shubhammalik06 <shubham.malik06@gmail.com>
2017-08-06 01:17:19 +0530

    Refresh Button and Menu item

--- app/src/main/java/com/example/shubhamr/gossipdevils/ChattingSection.java
+++ app/src/main/java/com/example/shubhamr/gossipdevils/ChattingSection.java
@@ -1,15 +1,25 @@
 package com.example.shubhamr.gossipdevils;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class ChattingSection extends AppCompatActivity {
 
     EditText message;
     ImageButton send,upload;
+    MenuItem item;
+    // Our created menu to use
+    private Menu mymenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,5 +44,45 @@ public class ChattingSection extends AppCompatActivity {
 
             }
         });
+
+        // Do animation start
+        LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        ImageView iv = (ImageView) inflater.inflate(R.layout.iv_refresh, null);
+        Animation rotation = AnimationUtils.loadAnimation(this, R.anim.rotate_refresh);
+        rotation.setRepeatCount(Animation.INFINITE);
+        iv.startAnimation(rotation);
+        item.setActionView(iv);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Add our menu
+        getMenuInflater().inflate(R.menu.main, menu);
+
+        // We should save our menu so we can use it to reset our updater.
+        mymenu = menu;
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_refresh:
+                new UpdateTask(this).execute();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    public void resetUpdating() {
+        // Get our refresh item from the menu
+        MenuItem m = mymenu.findItem(R.id.action_refresh);
+        if (m.getActionView() != null) {
+            // Remove the animation.
+            m.getActionView().clearAnimation();
+            m.setActionView(null);
+        }
     }
 }
